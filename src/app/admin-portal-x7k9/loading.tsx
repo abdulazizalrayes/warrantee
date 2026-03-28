df70c7c693f2bdb5c6d520027668c3a1b03c76e2
abdulazizalrayes <abdulaziz.alrayes@gmail.com>
2026-03-28 14:23:15 +0300

    chore: delete duplicate admin-portal-x7k9/loading.tsx outside [locale]

--- src/app/admin-portal-x7k9/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function AdminLoading() {
-  return (
-    <div className="min-h-screen bg-gray-950 flex items-center justify-center">
-      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-yellow-400"></div>
-    </div>
-  );
-}
