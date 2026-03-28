d6784516a855fe33edeac1774a25c0cab56b2373
abdulazizalrayes <abdulaziz.alrayes@gmail.com>
2026-03-28 17:10:56 +0300

    fix: rewrite Hotjar component to fix SWC compilation error

--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -5,24 +5,26 @@ import Script from "next/script";
 const HOTJAR_ID = process.env.NEXT_PUBLIC_HOTJAR_ID;
 
 export default function Hotjar() {
-  if (!HOTJAR_ID) return null;
+  if (!HOTJAR_ID) {
+    return null;
+  }
+
+  const snippet = [
+    "(function(h,o,t,j,a,r){",
+    "h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};",
+    "h._hjSettings={hjid:" + "HOTJAR_ID" + ",hjsv:6};",
+    "a=o.getElementsByTagName('head')[0];",
+    "r=o.createElement('script');r.async=1;",
+    "r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;",
+    "a.appendChild(r);",
+    "})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');",
+  ].join("\n").replace("HOTJAR_ID", HOTJAR_ID);
 
   return (
     <Script
       id="hotjar-script"
       strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: \`
-          (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:\${HOTJAR_ID},hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-          })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        \`,
-      }}
+      dangerouslySetInnerHTML={{ __html: snippet }}
     />
   );
 }
